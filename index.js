6178f025d7e13cdd662ebf97f0495ae6044acb66
KaterinaGrabovyk <katya.grabovik@ukr.net>
2025-06-11 17:24:13 +0300

    Patched some mistakes. Added update and delete functions.

--- index.js
+++ index.js
@@ -23,15 +23,9 @@ app.get("/", async (req, res) => {
 app.get("/add",async(req,res)=>{
     res.render("add.ejs");
 })
-
-app.get("/update",async(req,res)=>{
-    res.render("update.ejs");
-})
-
 app.post("/add",async(req,res)=>{
     try {
         const data=req.body;
-        console.log(data);
         const response = await axios.post("http://localhost:8000/products/add",data,{ headers: {"Content-Type": "application/x-www-form-urlencoded"}});
         console.log(response["data"]);
         res.redirect("/"); 
@@ -41,17 +35,56 @@ app.post("/add",async(req,res)=>{
     }       
 })
 
-app.post("/update",async(req,res)=>{
+app.get("/update/:id",async(req,res)=>{
     try {
-        const data=req.body;
-        console.log(data);
-        // res.redirect("/"); 
+        const id=req.params.id;
+        const product=await axios.get(`http://localhost:8000/products/${id}`);
+        res.render("update.ejs",{
+            item:product.data
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(`Item with id =${id} doesn't exist.`);
+    }     
+
+})
+app.post("/update/:id",async(req,res)=>{
+    try {
+        const id=req.params.id;
+        var response;
+        var filtered=Object.entries(req.body).filter(([key, value]) => value !== '');
+        if(filtered.length<3&&filtered.length!=0){
+            console.log(filtered)
+            response=await axios.patch(`http://localhost:8000/products/update/${id}`,Object.fromEntries(filtered),{ headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+            console.log(response["data"]);
+            res.redirect("/"); 
+        }else if(filtered.length===3){
+            console.log(req.body)
+             response=await axios.put(`http://localhost:8000/products/update/${id}`,req.body,{ headers: {"Content-Type": "application/x-www-form-urlencoded"}});
+             console.log(response["data"]);
+             res.redirect("/"); 
+        }
+        else{
+            console.log("No updates provided.");
+            res.redirect("/");
+        }; 
     } catch (err) {
         console.log(err);
         res.status(500).send("Error occurred");
     }  
 })
 
+app.get("/delete/:id",async (req,res)=>{
+    try {
+        const id=req.params.id;
+        const response=await axios.delete(`http://localhost:8000/products/delete/${id}`);
+        console.log(response["data"]);
+        res.redirect("/");
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(`Item with id =${id} doesn't exist.`);
+    }      
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
